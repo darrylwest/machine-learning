2f090fe7b0615123cdf8c2dd5e47b658970ac23b
darryl west <darryl.west@RainCitySoftware.com>
2016-10-05 12:55:13 -0700

    word count in python

--- python/count-words.py
+++ python/count-words.py
@@ -1,4 +1,3 @@
-
 """Count words."""
 
 def count_words(s, n):
@@ -14,25 +13,24 @@ def count_words(s, n):
             words[ word ] += 1
 
     # TODO: Sort the occurences in descending order (alphabetically in case of ties)
-    top_n = []
+    list = []
     for key in words.keys():
         val = words[ key ]
-        print key, val
-        tup = (key, val)
-        top_n.append( tup )
+        list.append( (key, val) )
 
-    print words
-    
-    
+    alpha = sorted(list, key=lambda x: x[0], reverse=False)
+
+    top_n = sorted(alpha, key=lambda x: x[1], reverse=True)
+
     # TODO: Return the top n words as a list of tuples (<word>, <count>)
 
-    return top_n
+    return top_n[ 0:n ]
 
 
 def test_run():
     """Test count_words() with some inputs."""
     print count_words("cat bat mat cat bat cat", 3)
-    """ print count_words("betty bought a bit of butter but the butter was bitter", 3) """
+    print count_words("betty bought a bit of butter but the butter was bitter", 3)
 
 
 if __name__ == '__main__':
