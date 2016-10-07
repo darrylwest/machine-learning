454e50445be4f488482995adab641074615d1327
darryl west <darryl.west@RainCitySoftware.com>
2016-10-06 18:18:26 -0700

    additional unit tests in separate file

--- machine-learning-ex1/ex1/testComputeCosts.m
+++ machine-learning-ex1/ex1/testComputeCosts.m
@@ -1,14 +1,15 @@
 function ok = testComputeCosts()
 
     ok = true;
-    check = 1E-9;
+    check = 1E-6;
 
     % X is the design matrix containing training data
     % y = is class label vector with the y values
 
     X = [1 2; 1 3; 1 4; 1 5];
-    y = [ 7; 6; 5; 4]; 
-    theta = [0.1;0.2];
+    y = [ 7; 6; 5; 4 ]; 
+    theta = [ 0.1 ; 0.2 ];
+
     cost = computeCost( X, y, theta );
 
     fprintf('cost %f\n', cost);
@@ -21,7 +22,7 @@ function ok = testComputeCosts()
 
     X = [ 1 2 3; 1 3 4; 1 4 5; 1 5 6];
     y = [ 7; 6; 5; 4 ]; 
-    theta = [ 0.1; 0.2; 0.3 ];
+    theta = [ 0.1; 0.2 ; 0.3 ];
     cost = computeCost( X, y, theta );
 
     fprintf('cost %f\n', cost);
@@ -31,7 +32,33 @@ function ok = testComputeCosts()
       whos;
       ok = false;
     end
+    
+    % from the housing example
+    X = [ 1 1 ; 1 2 ; 1 3 ];
+    y = [ 1 ; 2 ; 3 ]; 
+    theta = [ 0.0 ; 0.0 ];
+    cost = computeCost( X, y, theta );
 
+    fprintf('cost %f\n', cost);
+
+    if abs(cost - 2.333333) > check
+      fprintf('cost failed...');
+      whos;
+      ok = false;
+    end
+    
+    theta = [ 0 ; 1 ];
+    cost = computeCost( X, y, theta );
+
+    fprintf('cost %f\n', cost);
+
+    if abs(cost - 0.0) > check
+      fprintf('cost failed...');
+      whos;
+      ok = false;
+    end
+
+    % tests complete...
     if ok == true
       fprintf('all tests passed!\n');
     end
